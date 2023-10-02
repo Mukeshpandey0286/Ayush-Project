74ad97e0cd4c3f333c10ac04c2c3e569f47bc4df
flaamdeen <panditmukki5@gmail.com>
2023-10-02 16:02:34 +0530

    New commit

--- backend/jwtToken/jwtToken.js
+++ backend/jwtToken/jwtToken.js
@@ -1,4 +1,4 @@
-import JWT from "jsonwebtoken"
+import JWT from "jsonwebtoken";
 
 // we get jwt secret key or token:-
 
@@ -6,19 +6,24 @@ const SECRET_KEY = process.env.JWT_SECRET_KEY;
 const EXPIRE_COOKIE = process.env.EXPIRE_COOKIE;
 
 // Create a JWT when a user logs in
-export const createToken =async(user, res,statusCode) =>{
-    try {
-        const token = await JWT.sign({ id:user._id }, SECRET_KEY, { expiresIn: `${EXPIRE_COOKIE}d` });
-const options = {
-  expires: new Date(Date.now() + EXPIRE_COOKIE * 24 * 60 * 60 * 1000), // Cookie expiration time in milliseconds
-  httpOnly: true, 
-  }
-            // Set the token as a cookie with the specified expiration time
-    res.cookie('token', token,options );
+export const createToken = async (user, res, statusCode) => {
+  try {
+    const token = await JWT.sign({ id: user._id }, SECRET_KEY, {
+      expiresIn: `${EXPIRE_COOKIE}d`,
+    });
+    const options = {
+      expires: new Date(Date.now() + EXPIRE_COOKIE * 24 * 60 * 60 * 1000), // Cookie expiration time in milliseconds
+      httpOnly: true,
+    };
+    // Set the token as a cookie with the specified expiration time
+    res.cookie("token", token, options);
 
-    return res.status(statusCode).json({ success: true, user, message: "Token created and stored in a cookie successfully!" });
-
-    } catch (error) {
-        console.log("Failed to create token",error.message);
-    }
+    return res.status(statusCode).json({
+      success: true,
+      user,
+      message: "Login SuccessFull",
+    });
+  } catch (error) {
+    console.log("Failed to create token", error.message);
   }
+};
